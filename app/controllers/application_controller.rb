4a42e4036501a0d8655169f0e230fbcc8f8341cc
Vishal Pawle <vishal@techvision.net.in>
2016-02-15 09:15:53 -0800

    meta tags part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,44 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 #define set_header in application_controller.rb:
+  before_action :prepare_meta_tags, if: "request.get?"
+
+  def prepare_meta_tags(options={})
+    site_name   = "nfbmpune.org"
+    title       = [controller_name, action_name].join(" ")
+    description = "National federation of the blind is an NGO, of the blind, for the blind and by the blind. Pune region is the first regional unit of NFBM Maharashtra."
+#    image       = options[:image] || "your-default-image-url"
+    current_url = request.url
+
+    # Let's prepare a nice set of defaults
+    defaults = {
+      site:        site_name,
+      title:       title,
+#      image:       image,
+      description: description,
+      keywords:    %w[Pune blind federation],
+#      twitter: {
+#        site_name: site_name,
+#        site: '@thecookieshq',
+#        card: 'summary',
+#        description: description,
+#        image: image
+#      },
+      og: {
+        url: current_url,
+        site_name: site_name,
+        title: title,
+#        image: image,
+        description: description,
+        type: 'website'
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
+  end
+
   def set_header(p_type, filename)
     case p_type
       when 'xls'
---
+++ config/initializers/meta_tags.rb
@@ -0,0 +1,6 @@
+MetaTags.configure do |c|
+  c.title_limit        = 200
+  c.description_limit  = 260
+  c.keywords_limit     = 255
+  c.keywords_separator = ', '
+end
